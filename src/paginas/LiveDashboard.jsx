38670008f94036ed8e63a458d4c1b9ad32195785
Angela Monsalve <angelapmonsalve@gmail.com>
2025-09-18 16:50:58 -0500

    Crea componente LiveDashboard para mostrar gráficas

---
+++ src/paginas/LiveDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Este componente será el dashboard para mostrar las gráficas
+const LiveDashboard = () => {
+  return (
+    <div style={{ padding: 32 }}>
+      <h2>Dashboard de Gráficas en Vivo</h2>
+      {/* Aquí se mostrarán las gráficas usando Recharts */}
+    </div>
+  );
+};
+
+export default LiveDashboard;
